829409495d51a09d11dc37003cf3c249273552d5
Sean Kelliher <sbkelliher@icloud.com>
2020-03-28 12:31:23 -0400

    Add 'placeResult' function.

--- site.js
+++ site.js
@@ -103,5 +103,25 @@ gallery = {
         });
     },
 
+    placeResult: function (image) {
 
-}; //close "gallery" variable (let).
+        //Receive image. Get its source and alt.
+        const source = image.src;
+        const alternative = image.alt;
+
+        //Adjust image source to get source for its larger image.
+        //In other words, you're removing "-sm.jpg".
+        const sourceAdj = source.substr(0, source.length - 7) + ".jpg";
+
+        //Get image with figure class "large". Set its source and alt.
+        const large = document.querySelector("figure.large img");
+        large.src = sourceAdj;
+        large.alt = alternative;
+
+        //Get caption element. Set its text.
+        //const caption = document.querySelector("figure.large figcaption");
+        const caption = document.getElementById("caption");
+        caption.textContent = alternative;
+    }
+
+}; //close "gallery".
