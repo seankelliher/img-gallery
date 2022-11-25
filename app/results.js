00299d4f0d8e4632018c95c6ce3616d73a094ce8
Sean Kelliher <sbkelliher@icloud.com>
2022-11-25 15:03:42 -0500

    Minor styling change on comments.

--- app/results.js
+++ app/results.js
@@ -1,19 +1,19 @@
 function placeResult(image) {
 
-    //Receive image. Get its source and alt.
+    // Receive image. Get its source and alt.
     const source = image.src;
     const alternative = image.alt;
 
-    //Adjust image source to get source for its larger image.
-    //In other words, you're removing "-sm.jpg".
+    // Adjust image source to get source for its larger image.
+    // In other words, you're removing "-sm.jpg".
     const sourceAdj = source.substr(0, source.length - 7) + ".jpg";
 
-    //Get image with figure class "large". Set its source and alt.
+    // Get image with figure class "large". Set its source and alt.
     const large = document.querySelector("figure.large img");
     large.src = sourceAdj;
     large.alt = alternative;
 
-    //Get caption element. Set its text.
+    // Get caption element. Set its text.
     const caption = document.getElementById("caption");
     caption.textContent = alternative;
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {getFirstThumb} from "./app/thumbs.js";
 
-//When DOM loads, invoke function.
+// When DOM loads, invoke function.
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
