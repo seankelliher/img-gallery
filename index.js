07e77ffc2f78430492cde70b5ee0254c535d913c
Sean Kelliher <sbkelliher@icloud.com>
2023-08-26 15:06:59 -0400

    Rebuild app wit Vue. Removing these 'no longer needed' files.

--- index.js
+++
@@ -1,8 +0,0 @@
-import {getFirstThumb} from "./app/thumbs.js";
-
-// When DOM loads, invoke function.
-document.addEventListener("DOMContentLoaded", function () {
-    "use strict";
-
-    getFirstThumb();
-});
