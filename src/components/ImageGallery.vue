ca6414d175e67d3d47235e70fb58307b9d723ecd
Sean Kelliher <sbkelliher@icloud.com>
2026-08-16 13:54:33 -0400

    Add figcaption to large image. Remove p tag caption.

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -48,7 +48,6 @@ function opacity() {
 
 <template>
     <div id="container">
-
         <section class="large-box">
             <figure class="large">
                 <img
@@ -59,13 +58,14 @@ function opacity() {
                 <svg @click="prevArrow" @keyup.enter="prevArrow" id="previous-arrow" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path pointer-events="none" d="M0 0h24v24H0z" fill="none"/><path pointer-events="none" d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" stroke="#1a1c1e" tabindex="0"/></svg>
 
                 <svg @click="nextArrow" @keyup.enter="nextArrow" id="next-arrow" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path pointer-events="none" d="M0 0h24v24H0z" fill="none"/><path pointer-events="none" d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" stroke="#1a1c1e" tabindex="0"/></svg>
+                <figcaption id="caption">
+                    {{ images[`${count}`].desc }}
+                </figcaption>
             </figure>
         </section>
 
         <section class="caption-box">
-            <p id="caption">
-                {{ images[`${count}`].desc }}
-            </p>
+
         </section>
 
         <section class="thumbnails-box">
